63fde9cdd07331130fdaf4b9af5fb06af7b97066
Hasan Huda <hasanhuda037@gmail.com>
2023-02-06 14:02:43 -0500

    completed assignment

--- src/main/java/com/hasan/daikichiRoutes/DaikichiController.java
+++ src/main/java/com/hasan/daikichiRoutes/DaikichiController.java
@@ -6,5 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/daikichi")
 public class DaikichiController {
-	
+	@RequestMapping("")
+	public String index() {
+		return "Welcome!";
+	}
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in your endeavors!";
+	}
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas";
+	}
 }
